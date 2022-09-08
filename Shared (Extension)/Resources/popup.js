bbc813b06f7df4df9423c0527f51018a914728dd
Chamomile <tima.chamomile@gmail.com>
2022-09-08 12:06:20 +0300

    removing old code

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -8,40 +8,13 @@ function findTypes() {
 
 browser.runtime.onMessage.addListener((request) => {
     if (request.type == "Read types response") {
-//        let countDiv = document.getElementById("type_list");
+
         
         document.getElementById("INTJ").innerHTML = new "hi";
         
-//        var unique = request.types.filter(onlyUnique);
+
         var pageTypes = request.types;
         
-//        var filtered = unique.filter(function (el) {
-//            return el != null && el != "" && el != " ";
-//        });
-        
-        if (pageTypes != null && pageTypes.length > 0) {
-            
-//            document.getElementById('INTJ').innerHTML += `${dict['INTJ']}`;
-            
-//            var div = document.createElement('div');
-//            div.setAttribute('id', 'proList');
-            
-//            countDiv.appendChild(div);
-            
-//            pageTypes.forEach(renderProductList);
-            
-//            function renderProductList(element, index, arr) {
-//                var div2 = document.createElement('div');
-//                div2.setAttribute('class', 'item');
-//
-//                div.appendChild(div2);
-//
-//                div2.innerHTML=div2.innerHTML + element + ` ${dict[element]}`;
-            }
-        }
-        else {
-            countDiv.appendChild(document.createTextNode('No types here.'));
-        }
     }
 });
 
