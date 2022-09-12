1f57f25cbc61d6a66a430310f469681b67f24a47
Chamomile <tima.chamomile@gmail.com>
2022-09-12 09:59:21 +0300

    the table works

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -8,14 +8,22 @@ function findTypes() {
 
 browser.runtime.onMessage.addListener((request) => {
     if (request.type == "Read types response") {
-        
-        document.getElementById("INTJ").innerHTML = new "hi";
+
+        // This returns the array of all types on a page (duplicates included)
         var pageTypes = request.types;
+        
+        for (const type of pageTypes) {
+            // This increments the html number for each type by 1 on every occurence of that type
+            var typevalue = document.getElementById(type);
+            var number = typevalue.innerHTML;
+            number++;
+            typevalue.innerHTML = number;
+        }
         
     }
 });
 
-function onlyUnique(value, index, self) {
-    dict[value] += 1;
-    return self.indexOf(value) === index;
-}
+//function onlyUnique(value, index, self) {
+//    dict[value] += 1;
+//    return self.indexOf(value) === index;
+//}
